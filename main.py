b88109cb3ccabc1bfd74ffe82dc71928cafbf890
shaygover <govershay@gmail.com>
2020-02-23 18:07:21 +0200

    Fixed harcoded setting that caused a bug

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def main():
     settings.read_settings()
 
     # Get list of posts
-    list_of_posts = csvparser.post_csv_reader()
+    list_of_posts = csvparser.post_csv_reader(settings.settings_dict["posts_csv_name"])
 
     # Get list of media (pictures an so on)
-    list_of_media = csvparser.post_csv_reader()
+    list_of_media = csvparser.post_csv_reader(settings.settings_dict["media_csv_name"])
 
     # Get list of tags
     list_of_tags = csvparser.tag_csv_reader()
 
     # Get list of menu items
-    list_of_nav = csvparser.post_csv_reader()
+    list_of_nav = csvparser.post_csv_reader(settings.settings_dict["nav_csv_name"])
 
     # Send items to the formatter
     outputcreator.post_text_output(list_of_posts, settings.settings_dict["project"], "Posts and Pages")
